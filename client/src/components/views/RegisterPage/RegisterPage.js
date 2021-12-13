a19b940151211ab3b5ecb7f62b2d31acda89207a
sukhyunil <sukhyunil@naver.com>
2021-12-13 22:47:57 +0900

    lastest

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,11 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 
-function RegisterPage() {
+function RegisterPage(props) {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Name, setName] = useState("")
+    const [Password, setPassword] = useState("")
+    const [ConfirmPassword, setConfirmPassword] = useState("")
+
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onNameHandler = (event) => {
+        setName(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onConfrimPasswordHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        console.log('Email', Email)
+        console.log('Password', Password)
+
+        if (Password !== ConfirmPassword){
+            return alert('비밀번호와 비밀번호 확은 같아야 합니다.')
+        }
+        let body = {
+            email: Email,
+            password: Password,
+            name:Name
+        }
+
+        // Axios.post('/api/users/register', body)
+
+        dispatch(registerUser(body))
+            .then(response => {
+                if(response.payload.success){
+                    props.history.push("/login")
+                }else{
+                    alert("Failed to sign up")
+                }
+            })
+
+        // action으로 이동
+        // Axios.post('/api/users/login', body)
+        // .then(response =>{
+
+        // })
+
+    }
+
+
     return (
-        <div>
-            
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+
+                <label>Name</label>
+                <input type="text" value={Name} onChange={onNameHandler} />
+
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+
+                <label>Confirm Password</label>
+                <input type="password" value={ConfirmPassword} onChange={onConfrimPasswordHandler} />
+
+                <br />
+                <button type="submit">
+                    회원가입
+                </button>
+            </form>
         </div>
     )
 }
 
-export default RegisterPage
+export default withRouter(RegisterPage)
+// export default RegisterPage
+
